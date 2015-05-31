292937f84e43661851094363ea248bd69ffd55c2
John Nickell <email@johnnickell.com>
2015-05-30 19:42:21 -0500

    create dir if not present

--- script/02-sublime-text.sh
+++ script/02-sublime-text.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 if [ ! -f "$HOME/.config/sublime-text-3/Installed Packages/Package Control.sublime-package" ]; then
+    if [ ! -d "$HOME/.config/sublime-text-3/Installed Packages" ]; then
+        mkdir -p "$HOME/.config/sublime-text-3/Installed Packages"
+    fi
     cd "$HOME/.config/sublime-text-3/Installed Packages"
     wget -O "Package Control.sublime-package" "https://packagecontrol.io/Package%20Control.sublime-package"
 fi
